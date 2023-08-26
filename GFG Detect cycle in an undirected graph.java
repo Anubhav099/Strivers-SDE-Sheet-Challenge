1290083a30f812a1f582e2f35aa05a59c999bb4b
Anubhav099 <ait.anubhav@gmail.com>
2023-08-26 10:17:17 +0530

    26:08:2023

--- GFG Detect cycle in an undirected graph.java
+++ GFG Detect cycle in an undirected graph.java
@@ -1,3 +1,5 @@
+// DFS approach
+
 class Solution {
     private class Pair {
         int node;
@@ -14,11 +16,11 @@ class Solution {
         boolean[] vis = new boolean[V];
         for (int i = 0; i < V; i++)
             if (!vis[i])
-                if (help(i, adj, vis)) return true;
+                if (bfs(i, adj, vis)) return true;
         return false;
     }
     
-    private boolean help(int cur, ArrayList<ArrayList<Integer>> adj, boolean[] vis) {
+    private boolean bfs(int cur, ArrayList<ArrayList<Integer>> adj, boolean[] vis) {
         Queue<Pair> q = new LinkedList<>();
         
         vis[cur] = true;
@@ -35,6 +37,45 @@ class Solution {
                 } else if(i != parent)
                     return true;
         }
+        return false;
+    }
+}
+
+
+// BFS Approach
+
+class Solution {
+    private class Pair {
+        int node;
+        int parent;
+        Pair(int node, int parent) {
+            this.node = node;
+            this.parent = parent;
+        }
+    }
+    
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        boolean[] vis = new boolean[V];
+        Set<Integer> set = new HashSet<>();
+        
+        for (int i = 0; i < V; i++)
+            if (!vis[i])
+                if (dfs(new Pair(i, -1), adj, vis, set)) return true;
+                
+        return false;
+    }
+    
+    private boolean dfs(Pair cur, ArrayList<ArrayList<Integer>> adj, boolean[] vis, Set<Integer> set) {
+        if (!set.add(cur.node)) return true;
+        
+        vis[cur.node] = true;
+        for (int i: adj.get(cur.node))
+            if (i != cur.parent)
+                if(dfs(new Pair(i, cur.node), adj, vis, set)) return true;
+                
+        set.remove(cur.node);
         return false;
     }
 }
